9c23c83b1bc9b71e4b086d7621af39f83f997b15
Aminul Islam Mahim <104668744+Aminulislammahim23@users.noreply.github.com>
2026-01-27 13:50:43 +0600

    fix link header and footer

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  require_once './app/views/layouts/header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+    <link rel="stylesheet" href="app/assets/css/index.css">
+    <script src="app/assets/js/index.js" defer></script>
+</head>
+<body>
+    <main>
+        <p>This is the main content of the page.</p>
+    </main>
+    
+</body>
+</html>
+<?php
+    require_once './app/views/layouts/footer.php';
+?>
